813aa0e7fe4d783ad79f8ddec9fa62ca46749a0f
Rafael Conrado <rafael.conrado.farias@gmail.com>
2017-11-05 21:31:48 -0200

    add touch support

--- src/SwipeJS.js
+++ src/SwipeJS.js
@@ -2,109 +2,144 @@
 
 class SwipeJS {
 
-    construct() {
-        this.swipeDirection = null;
-        this.swipeClicked = true;
-        this.swipeStarted = false;
-        this.swipeElement = null;
-        this.currentX = 0;
-        this.currentY = 0;
-        this.startX = 0;
-        this.startY = 0;
+  construct() {
+    this.swipeDirection = null;
+    this.swipeClicked = true;
+    this.swipeStarted = false;
+    this.swipeElement = null;
+    this.currentX = 0;
+    this.currentY = 0;
+    this.startX = 0;
+    this.startY = 0;
+  }
+
+  addMouseListeners() {
+    this.mouseDownListener();
+    this.mouseUpListener();
+    this.mouseMoveListener();
+  }
+
+  reset() {
+    this.swipeDirection = null;
+    this.swipeClicked = false;
+    this.swipeElement = null;
+  }
+
+  mouseDownListener() {
+    document.body.addEventListener('mousedown', (evt) => {
+      this.down(evt, this)
+    });
+    document.body.addEventListener('touchstart', (evt) => {
+      this.down(evt, this)
+    });
+  }
+
+  mouseUpListener() {
+    document.body.addEventListener('mouseup', (evt) => {
+      this.up(evt, this)
+    });
+    document.body.addEventListener('touchend', (evt) => {
+      this.up(evt, this)
+    });
+  }
+
+  up(evt, self) {
+    if (self.swipeClicked && self.swipeDirection) {
+      self.swipeElement.dispatchEvent(new CustomEvent('swipe.end', {
+        bubbles: true,
+        detail: {
+          direction: self.swipeDirection,
+          x: self.currentX,
+          y: self.currentY,
+        }
+      }));
+    } else if (self.swipeClicked && !self.swipeDirection) {
+      self.swipeElement.dispatchEvent(new CustomEvent('swipe.cancel', {
+        bubbles: true
+      }));
     }
 
-    addMouseListeners() {
-        this.mouseDownListener();
-        this.mouseUpListener();
-        this.mouseMoveListener();
+    self.reset();
+  }
+
+  down(evt, self) {
+    self.swipeClicked = true;
+    self.swipeElement = evt.srcElement;
+    if(evt instanceof TouchEvent) {
+      self.startX = evt.changedTouches[0].clientX;
+      self.startY = evt.changedTouches[0].clientY;
+    } else {
+      self.startX = evt.x;
+      self.startY = evt.y;
     }
+  }
 
-    reset() {
-        this.swipeDirection = null;
-        this.swipeClicked = false;
-        this.swipeElement = null;
+  move(evt, self) {
+    if (self.swipeClicked) {
+      if(evt instanceof TouchEvent) {
+        self.currentX = evt.changedTouches[0].clientX;
+        self.currentY = evt.changedTouches[0].clientY;
+      } else {
+        self.currentX = evt.x;
+        self.currentY = evt.y;
+      }
+
+
+      let diffX = self.currentX - self.startX;
+      let diffY = self.currentY - self.startY;
+
+      if (diffX > SwipeJSConfig.sensitivityX) {
+        if (diffY > SwipeJSConfig.sensitivityY) {
+          self.swipeDirection = SwipeJSConfig.SOUTH_EAST;
+        } else if (diffY < -SwipeJSConfig.sensitivityY) {
+          self.swipeDirection = SwipeJSConfig.NORTH_EAST;
+        } else {
+          self.swipeDirection = SwipeJSConfig.EAST;
+        }
+      } else if (diffX < -SwipeJSConfig.sensitivityX) {
+        if (diffY > SwipeJSConfig.sensitivityY) {
+          self.swipeDirection = SwipeJSConfig.SOUTH_WEST;
+        } else if (diffY < -SwipeJSConfig.sensitivityY) {
+          self.swipeDirection = SwipeJSConfig.NORTH_WEST;
+        } else {
+          self.swipeDirection = SwipeJSConfig.WEST;
+        }
+      } else {
+        if (diffY > SwipeJSConfig.sensitivityY)
+          self.swipeDirection = SwipeJSConfig.SOUTH;
+        else if (diffY < -SwipeJSConfig.sensitivityY)
+          self.swipeDirection = SwipeJSConfig.NORTH;
+        else {
+          self.swipeDirection = null;
+        }
+      }
+
+      if (!self.swipeStarted) {
+        self.swipeStarted = true;
+        self.swipeElement.dispatchEvent(new CustomEvent('swipe.start', {
+          bubbles: true
+        }));
+      } else {
+        self.swipeElement.dispatchEvent(new CustomEvent('swipe.progress', {
+          bubbles: true,
+          detail: {
+            direction: self.swipeDirection,
+            x: self.currentX,
+            y: self.currentY,
+          }
+        }));
+      }
     }
+  }
 
-    mouseDownListener() {
-        var self = this;
-        document.body.addEventListener('mousedown', function (evt) {
-            self.swipeClicked = true;
-            self.swipeElement = evt.srcElement;
-            self.startX = evt.x;
-            self.startY = evt.y;
-        });
-    }
-
-    mouseUpListener() {
-        var self = this;
-        document.body.addEventListener('mouseup', function (evt) {
-            if (self.swipeClicked && self.swipeDirection) {
-                self.swipeElement.dispatchEvent(new CustomEvent('swipe.end', {
-                    detail: {
-                        direction: self.swipeDirection,
-                        x: self.currentX,
-                        y: self.currentY,
-                    }
-                }));
-            } else if (self.swipeClicked && !self.swipeDirection) {
-                self.swipeElement.dispatchEvent(new CustomEvent('swipe.cancel'));
-            }
-
-            self.reset();
-        });
-    }
-
-    mouseMoveListener() {
-        var self = this;
-        document.body.addEventListener('mousemove', function (evt) {
-            if (self.swipeClicked) {
-                self.currentX = evt.x;
-                self.currentY = evt.y;
-
-                let diffX = self.currentX - self.startX;
-                let diffY = self.currentY - self.startY;
-
-                if (diffX > SwipeJSConfig.sensitivityX) {
-                    if (diffY > SwipeJSConfig.sensitivityY) {
-                        self.swipeDirection = SwipeJSConfig.SOUTH_EAST;
-                    } else if (diffY < -SwipeJSConfig.sensitivityY) {
-                        self.swipeDirection = SwipeJSConfig.NORTH_EAST;
-                    } else {
-                        self.swipeDirection = SwipeJSConfig.EAST;
-                    }
-                } else if (diffX < -SwipeJSConfig.sensitivityX) {
-                    if (diffY > SwipeJSConfig.sensitivityY) {
-                        self.swipeDirection = SwipeJSConfig.SOUTH_WEST;
-                    } else if (diffY < -SwipeJSConfig.sensitivityY) {
-                        self.swipeDirection = SwipeJSConfig.NORTH_WEST;
-                    } else {
-                        self.swipeDirection = SwipeJSConfig.WEST;
-                    }
-                } else {
-                    if (diffY > SwipeJSConfig.sensitivityY)
-                        self.swipeDirection = SwipeJSConfig.SOUTH;
-                    else if (diffY < -SwipeJSConfig.sensitivityY)
-                        self.swipeDirection = SwipeJSConfig.NORTH;
-                    else {
-                        self.swipeDirection = null;
-                    }
-                }
-
-                if (!self.swipeStarted) {
-                    self.swipeStarted = true;
-                    self.swipeElement.dispatchEvent(new CustomEvent('swipe.start'));
-                } else {
-                    self.swipeElement.dispatchEvent(new CustomEvent('swipe.progress', {
-                        detail: {
-                            direction: self.swipeDirection,
-                            x: self.currentX,
-                            y: self.currentY,
-                        }
-                    }));
-                }
-            }
-        });
-    }
+  mouseMoveListener() {
+    document.body.addEventListener('mousemove', (evt) => {
+      this.move(evt, this)
+    });
+    document.body.addEventListener('touchmove', (evt) => {
+      this.move(evt, this)
+    });
+  }
 }
 
 exports.SwipeJS = new SwipeJS;
